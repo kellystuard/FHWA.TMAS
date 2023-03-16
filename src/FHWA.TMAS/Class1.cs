c4f2dbbb7624a84fda5829ead423ba8fffb340a6
Kelly Stuard <kellystuard@gmail.com>
2023-03-16 15:19:00 -0500

    Initial commit

---
+++ src/FHWA.TMAS/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace FHWA.TMAS;
+
+/// <summary>
+/// Default class included in class library template.
+/// </summary>
+public class Class1
+{
+
+}
---
+++ test/FHWA.TMAS.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FHWA.TMAS.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
